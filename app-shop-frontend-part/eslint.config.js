08a7f76b09c085bb1d245738b4575e184d207920
Egor <leskovegor490@gmail.com>
2025-08-09 16:55:22 +0300

    settings finish

--- app-shop-frontend-part/eslint.config.js
+++ app-shop-frontend-part/eslint.config.js
@@ -1,30 +1,77 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      prettier: prettierPlugin
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
+      ...prettierPlugin.configs.recommended.rules,
       'no-console': 'warn',
       'eqeqeq': 'warn',
       'curly': 'warn',
       'no-else-return': 'warn',
       'default-case': 'warn',
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            'internal',
+            'sibling',
+            'parent',
+            'index',
+          ],
+          pathGroups: [
+            {
+              pattern: '@shared/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@features/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@entities/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@widgets/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+      'import/no-duplicates': 'error',
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/namespace': 'error',
     },
   },
 ])
--- app-shop-frontend-part/vite.config.ts
+++ app-shop-frontend-part/vite.config.ts
@@ -5,16 +5,16 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@app': path.resolve(__dirname, './src/app'),
-			'@pages': path.resolve(__dirname, './src/pages'),
-			'@widgets': path.resolve(__dirname, './src/widgets'),
-			'@features': path.resolve(__dirname, './src/features'),
-			'@entities': path.resolve(__dirname, './src/entities'),
-			'@shared': path.resolve(__dirname, './src/shared'),
-		},
-	},
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 })
