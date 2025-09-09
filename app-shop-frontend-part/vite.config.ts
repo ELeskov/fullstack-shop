9f07f055cf3fb8757bbaa698bba0513c7d4d81e9
Egor <leskovegor490@gmail.com>
2025-09-09 17:58:25 +0300

    dark theme in link

--- app-shop-frontend-part/vite.config.ts
+++ app-shop-frontend-part/vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: { host: true, port: 8080 },
   css: {
     preprocessorOptions: {
       scss: {
