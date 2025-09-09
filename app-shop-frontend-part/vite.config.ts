f1455ec7886f249ef09a0434faebe01a582de93f
Egor <EgorPupunya@Air-Alena.Dlink>
2025-09-09 17:54:49 +0300

    like page

--- app-shop-frontend-part/vite.config.ts
+++ app-shop-frontend-part/vite.config.ts
@@ -8,6 +8,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    host: true,
+    port: 8080,
+  },
   css: {
     preprocessorOptions: {
       scss: {
