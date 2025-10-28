1e5a24de516ee120c7aed996b019a6c710de4366
Egor <leskovegor490@gmail.com>
2025-10-28 17:28:46 +0300

    add react-hook-form on page createProduct

--- app-shop-frontend-part/src/widgets/createNewProductForm/ui/createNewProductForm.tsx
+++ app-shop-frontend-part/src/widgets/createNewProductForm/ui/createNewProductForm.tsx
@@ -1,3 +1,5 @@
+import { Controller, type SubmitHandler, useForm } from 'react-hook-form'
+
 import { ImagePlus } from 'lucide-react'
 
 import { Button } from '@/shared/ui/components/ui/button'
@@ -19,9 +21,28 @@ import {
 
 import s from './createNewProductForm.module.scss'
 
+interface Form {
+  title: string
+  price: number
+  category: string
+  color: string
+}
 export function CreateNewProductForm() {
+  const { register, handleSubmit, control } = useForm<Form>({
+    defaultValues: {
+      title: '',
+      price: 1,
+      category: '',
+      color: '',
+    },
+  })
+
+  const onSubmit: SubmitHandler<Form> = (data) => console.log(data)
   return (
-    <form className={s['create-new-product-form']}>
+    <form
+      className={s['create-new-product-form']}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <FieldGroup>
         <FieldSet>
           <FieldGroup>
@@ -47,6 +68,7 @@ export function CreateNewProductForm() {
             <Field>
               <FieldLabel htmlFor="product-title">Название</FieldLabel>
               <Input
+                {...register('title')}
                 id="product-title"
                 name="title"
                 placeholder="Смартфон Apple iPhone 17 Pro 256 ГБ синий"
@@ -57,12 +79,12 @@ export function CreateNewProductForm() {
             <Field>
               <FieldLabel htmlFor="product-price">Цена (руб)</FieldLabel>
               <Input
+                {...register('price')}
                 id="product-price"
                 name="price"
                 type="number"
                 placeholder="128 999 ₽"
                 min={1}
-                defaultValue={1}
                 required
               />
             </Field>
@@ -73,47 +95,73 @@ export function CreateNewProductForm() {
           <FieldGroup className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <Field>
               <FieldLabel htmlFor="product-category">Категория</FieldLabel>
-              <Select name="product-category" required>
-                <SelectTrigger id="product-category">
-                  <SelectValue placeholder="Выберите категорию" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="smartphones">Смартфоны</SelectItem>
-                  <SelectItem value="laptops">Ноутбуки</SelectItem>
-                  <SelectItem value="tablets">Планшеты</SelectItem>
-                  <SelectItem value="headphones">Наушники</SelectItem>
-                  <SelectItem value="smartwatches">Смарт-часы</SelectItem>
-                  <SelectItem value="accessories">Аксессуары</SelectItem>
-                  <SelectItem value="audio">Аудиотехника</SelectItem>
-                  <SelectItem value="tv">Телевизоры</SelectItem>
-                  <SelectItem value="photo">Фото и видео</SelectItem>
-                  <SelectItem value="gaming">Игры и приставки</SelectItem>
-                  <SelectItem value="appliances">Бытовая техника</SelectItem>
-                  <SelectItem value="other">Другое</SelectItem>
-                </SelectContent>
-              </Select>
+              <Controller
+                control={control}
+                name="category"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    name="product-category"
+                    required
+                  >
+                    <SelectTrigger id="product-category">
+                      <SelectValue placeholder="Выберите категорию" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="smartphones">Смартфоны</SelectItem>
+                      <SelectItem value="laptops">Ноутбуки</SelectItem>
+                      <SelectItem value="tablets">Планшеты</SelectItem>
+                      <SelectItem value="headphones">Наушники</SelectItem>
+                      <SelectItem value="smartwatches">Смарт-часы</SelectItem>
+                      <SelectItem value="accessories">Аксессуары</SelectItem>
+                      <SelectItem value="audio">Аудиотехника</SelectItem>
+                      <SelectItem value="tv">Телевизоры</SelectItem>
+                      <SelectItem value="photo">Фото и видео</SelectItem>
+                      <SelectItem value="gaming">Игры и приставки</SelectItem>
+                      <SelectItem value="appliances">
+                        Бытовая техника
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
             </Field>
             <Field>
               <FieldLabel htmlFor="product-color">Цвет</FieldLabel>
-              <Select name="product-color" required>
-                <SelectTrigger id="product-color">
-                  <SelectValue placeholder="Выберите цвет" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="black">Чёрный</SelectItem>
-                  <SelectItem value="white">Белый</SelectItem>
-                  <SelectItem value="silver">Серебристый</SelectItem>
-                  <SelectItem value="space-gray">Космический серый</SelectItem>
-                  <SelectItem value="gold">Золотой</SelectItem>
-                  <SelectItem value="rose-gold">Розовое золото</SelectItem>
-                  <SelectItem value="blue">Синий</SelectItem>
-                  <SelectItem value="red">Красный</SelectItem>
-                  <SelectItem value="green">Зелёный</SelectItem>
-                  <SelectItem value="purple">Фиолетовый</SelectItem>
-                  <SelectItem value="yellow">Жёлтый</SelectItem>
-                  <SelectItem value="pink">Розовый</SelectItem>
-                </SelectContent>
-              </Select>
+
+              <Controller
+                control={control}
+                name="color"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    name="product-color"
+                    required
+                  >
+                    <SelectTrigger id="product-color">
+                      <SelectValue placeholder="Выберите цвет" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="black">Чёрный</SelectItem>
+                      <SelectItem value="white">Белый</SelectItem>
+                      <SelectItem value="silver">Серебристый</SelectItem>
+                      <SelectItem value="space-gray">
+                        Космический серый
+                      </SelectItem>
+                      <SelectItem value="gold">Золотой</SelectItem>
+                      <SelectItem value="rose-gold">Розовое золото</SelectItem>
+                      <SelectItem value="blue">Синий</SelectItem>
+                      <SelectItem value="red">Красный</SelectItem>
+                      <SelectItem value="green">Зелёный</SelectItem>
+                      <SelectItem value="purple">Фиолетовый</SelectItem>
+                      <SelectItem value="yellow">Жёлтый</SelectItem>
+                      <SelectItem value="pink">Розовый</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
             </Field>
           </FieldGroup>
         </FieldSet>
